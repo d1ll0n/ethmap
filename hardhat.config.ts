ca67a046d3fb9b3a8c3520e6cb69aa10ed364122
d1ll0n <dillonkellar@protonmail.com>
2021-08-19 04:44:18 -0500

    set baseFeePergas

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -39,7 +39,8 @@ export default {
       forking: {
         url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
         blockNumber: 12667185
-      }
+      },
+      initialBaseFeePerGas: 0
     },
     mainnet: {
       ...configureNetwork('mainnet', 1),
